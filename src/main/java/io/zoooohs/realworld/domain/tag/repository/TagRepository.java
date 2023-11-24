e69db1e53d4dbbb8d7f917c245160b528a612208
zoooo-hs <dogfooter219@gmail.com>
2023-11-25 00:46:20 +0900

    clean the project. restart

--- src/main/java/io/zoooohs/realworld/domain/tag/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package io.zoooohs.realworld.domain.tag.repository;
-
-import io.zoooohs.realworld.domain.tag.entity.ArticleTagRelationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends JpaRepository<ArticleTagRelationEntity, Long> {
-}
--- src/main/java/io/zoooohs/realworld/domain/tag/service/TagService.java
+++
@@ -1,7 +0,0 @@
-package io.zoooohs.realworld.domain.tag.service;
-
-import java.util.List;
-
-public interface TagService {
-    List<String> listOfTags();
-}
